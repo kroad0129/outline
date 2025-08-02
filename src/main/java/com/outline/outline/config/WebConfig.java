f0193dedc31e35d6b04da6fe8b0538655f4cac5b
kroad0129 <kroad0129@naver.com>
2025-08-02 20:18:46 +0900

    핫픽스

--- src/main/java/com/outline/outline/config/WebConfig.java
+++ src/main/java/com/outline/outline/config/WebConfig.java
@@ -1,14 +1,16 @@
 package com.outline.outline.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // Docker 컨테이너 내부에서 /uploads 디렉토리를 정적 자원으로 제공
         registry.addResourceHandler("/uploads/**")
-                .addResourceLocations("file:/home/ubuntu/outline/uploads/");
+                .addResourceLocations("file:/uploads/");
     }
 }
